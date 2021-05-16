5bce86d16522fbc1cbd1626a8facca16648854b3
cyr <cyr@zolotarev.pp.ua>
2021-05-16 13:00:00 +0300

    modules

---
+++ python_lessons/python_advanced_02_tic_tac_toe/main.py
@@ -0,0 +1,7 @@
+from win import Win
+from settings import NUMBERS_MAP
+
+if __name__ == "__main__":
+    win = Win(NUMBERS_MAP)
+
+    print(win.combinations)
--- python_lessons/python_advanced_02_tic_tac_toe/table.py
+++ python_lessons/python_advanced_02_tic_tac_toe/table.py
@@ -1,2 +1,7 @@
-from PrettyTable import PrettyTable
+import termtables as tt
+from settings import NUMBERS_MAP
+
+if __name__ == "__main__":
+    string = tt.to_string(NUMBERS_MAP, padding=(1, 3))
+    print(string)
 
