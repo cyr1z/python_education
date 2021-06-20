c6f18c7116e799b624776f72a2e17e157bdfeedf
cyr <cyr@zolotarev.pp.ua>
2021-06-20 17:27:08 +0300

    2.7 database practice - add (for loop) procedure - add random producs to card

--- databases/practice_2_7/02_procedures.sql
+++ databases/practice_2_7/02_procedures.sql
@@ -1,6 +1,42 @@
-
 -- 2. Написать 2 любые хранимые процедуры с использованием
 --    условий, циклов и транзакций.
+
+CREATE OR REPLACE
+    PROCEDURE add_random_products_to_cart(cart_id INT)
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    query        VARCHAR := 'INSERT INTO cart_products VALUES (%s, %s);';
+    result_query VARCHAR;
+    table_record RECORD;
+BEGIN
+    FOR table_record IN
+        (SELECT product_id
+        FROM products
+        WHERE random() < 0.01
+        LIMIT 50)
+        LOOP
+            result_query := CONCAT(result_query, format(query, cart_id, table_record.product_id));
+        END LOOP;
+    EXECUTE result_query;
+END;
+$$;
+
+
+BEGIN;
+
+CALL add_random_products_to_cart(17);
+
+SELECT *
+FROM cart_products
+WHERE carts_cart_id = 17;
+
+ROLLBACK;
+
+DROP PROCEDURE add_random_products_to_cart;
+
+
 
 CREATE MATERIALIZED VIEW city_orders_total
 AS
@@ -42,20 +78,20 @@ CALL update_city_orders_total();
 
 
 
-
 CREATE OR REPLACE
     PROCEDURE rotate_logs()
     LANGUAGE plpgsql
 AS
 $$
 DECLARE
-actual_count int;
+    actual_count int;
 BEGIN
     DELETE
     FROM price_change_log
     where datetime < CURRENT_DATE - INTERVAL '90 DAY';
     IF 1000 > (SELECT COUNT(*) FROM price_change_log)
-        THEN ROLLBACK;
+    THEN
+        ROLLBACK;
     ELSE
         COMMIT;
     END IF;
